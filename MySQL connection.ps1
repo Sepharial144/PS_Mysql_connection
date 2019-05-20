49f0348dd3778d7e0b2c9a9db64605074d45e543
Иващенко Дмитрий Андреевич <ida@ckb55.local>
2019-05-20 14:05:40 +0600

    Create MySQL connection.ps1
    Example mysql connection with execute data from database

---
+++ MySQL connection.ps1
@@ -0,0 +1,63 @@
+﻿# Connection variables 
+$user = 'power' 
+$pass = 'Shell144##!' 
+$database = 'ckb_main' 
+$MySQLHost = '192.168.40.57'
+$Global:connMySQL ="server=" + $MySQLHost + ";port=3306;uid=" + $user + ";pwd=" + $pass + ";database="+$database+";Pooling=FALSE"
+
+#-------------------------------------------------------Functions-----------------------------------------------------------------
+
+# Connection to MySQL Server
+function Connect-MySQL([string]$user,[string]$pass,[string]$MySQLHost,[string]$database) {
+  # Load MySQL .NET Connector Objects 
+  [void][system.reflection.Assembly]::LoadWithPartialName("MySql.Data") 
+
+  # Open Connection 
+  $conn = New-Object MySql.Data.MySqlClient.MySqlConnection($connMySQL) 
+  $conn.Open() 
+  return $conn 
+} 
+
+# Erase connect
+function Disconnect-MySQL($conn) {
+  $conn.Close()
+}
+
+# Query
+function Execute-MySQLQuery([string]$query) { 
+  # NonQuery - Insert/Update/Delete query where no return data is required
+  $cmd = New-Object MySql.Data.MySqlClient.MySqlCommand($query, $connMySQL)    # Create SQL command
+  $dataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($cmd)      # Create data adapter from query command
+  $dataSet = New-Object System.Data.DataSet                                    # Create dataset
+  $dataAdapter.Fill($dataSet, "data")                                          # Fill dataset from data adapter, with name "data"              
+  $cmd.Dispose()
+  return $dataSet.Tables["data"]                                               # Returns an array of results
+}
+
+#-------------------------------------------------------Point of start program----------------------------------------------------
+
+# Initiate connect
+$Connection = Connect-MySQL($user, $pass, $MySQLHost, $database)
+
+# Form query
+$query = "SELECT id, surname, name, patronymic FROM users;"
+
+# Execute data
+$Request = Execute-MySQLQuery $query
+
+
+# Get count of rows of result
+$countRows = $Request.rows.count
+
+# Show result
+for ($i = 0; $i -lt $countRows; $i++) {
+
+    Write-host $Request[$i].id
+    Write-host $Request[$i].surname
+    Write-host $Request[$i].name
+    Write-host $Request[$i].patronymic
+
+}
+
+# Disconnect
+Disconnect-MySQL($Connection)
